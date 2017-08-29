5fe53c759d92c9c99a2863bc9909b223d9994c80
Kalle Lindström <karl@dewire.com>
2017-08-29 12:18:30 +0200

    Updated dependencies and React Native

--- e2e/login_test.spec.js
+++ e2e/login_test.spec.js
@@ -12,16 +12,16 @@ describe('Example', () => {
   });
 
   it('should show the login button when the wrong credentials are entered', async () => {
-    await element(by.id('usernameInput')).replaceText('ausername');
-    await element(by.id('passwordInput')).replaceText('wrongpassword');
+    await element(by.id('usernameInput')).typeText('ausername');
+    await element(by.id('passwordInput')).typeText('wrongpassword\n');
     await element(by.id('loginButton')).tap();
     await expect(element(by.id('loginButton'))).toBeVisible();
   });
 
   it('should show the gists page on a successful login', async () => {
     await device.reloadReactNative();
-    await element(by.id('usernameInput')).replaceText(process.env.DEHUB_USERNAME);
-    await element(by.id('passwordInput')).replaceText(process.env.DEHUB_PASSWORD);
+    await element(by.id('usernameInput')).typeText(process.env.DEHUB_USERNAME);
+    await element(by.id('passwordInput')).typeText(`${process.env.DEHUB_PASSWORD}\n`);
     await element(by.id('loginButton')).tap();
     await expect(element(by.id('gistsContainer'))).toBeVisible();
   });
--- src/app_components/section_header.js
+++ src/app_components/section_header.js
@@ -1,5 +1,6 @@
 
-import React, { PropTypes } from 'react';
+import PropTypes from 'prop-types';
+import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import * as globalStyles from '../styles/global';
 
